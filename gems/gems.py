1e2b233523730a51b5e72cfde68b45f20a781312
d-pettersson <david.pettersson@code.berlin>
2020-05-28 14:52:56 +0200

    small refinements to gems.py

--- gems/gems.py
+++ gems/gems.py
@@ -1,15 +1,12 @@
 def get_input():
     a, b = map(int, input('Please input the first gem\nand its replacement(separated by a whitespace): ').strip().split())
-    if not check_input(a, b):
-        print('invalid move')
-    else:
-        print('valid move')
+    return a, b
 
-def check_input(a, b):
+def move_is_valid(a, b):
     return 0 if abs(a - b) > 3 else abs(a - b) % 2
 
 def print_welcome_message():
-    msg = 'Welcome to the sparkling diamonds game.\nThe game\'s layout is:\n1 2 3\n4 5 6\n7 8 9\n\nThis is your board:'
+    msg = 'Welcome to the sparkling diamonds game.\nThe game\'s layout is:\n1 2 3\n4 5 6\n7 8 9\n\nThis is your board:\n'
     print(msg)
 
 def print_board(board):
@@ -20,7 +17,11 @@ def main():
     initial_board = [1, 2, 3, 4, 4, 5, 5, 3, 4]
     print_welcome_message()
     print_board(initial_board)
-    get_input()
+    a, b = get_input()
+    if move_is_valid(a, b):
+        print('valid move')
+    else:
+        print('invalid move')
 
 if __name__ == '__main__':
     main()
